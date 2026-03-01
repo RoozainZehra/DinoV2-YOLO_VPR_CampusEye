85cca0a000b551db519c1a356db184641a1d635b
HibaShahidA <hs08036@st.habib.edu.pk>
2026-03-01 06:10:34 +0500

    path name fixed

--- api.py
+++ api.py
@@ -18,7 +18,7 @@ app = FastAPI()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-VPR_DIR = os.path.join(BASE_DIR, "vpr")
+VPR_DIR = os.path.join(BASE_DIR, "VPR")
 
 print("Loading VPR model...")
 
